16e219978793fdf8bd423e537ab4d6ce402e12c3
Fayne Aldan <FayneAldan@gmail.com>
2022-12-29 18:57:13 -0700

    Remove deprecated scripts

--- convertColor.ts
+++
@@ -1,5 +0,0 @@
-/**
- * @deprecated Please use cli/convertColor.ts instead
- */
-
-import "./cli/convertColor.ts";
--- migrate.ts
+++
@@ -1,5 +0,0 @@
-/**
- * @deprecated Please use cli/migrate.ts instead
- */
-
-import "./cli/migrate.ts";
