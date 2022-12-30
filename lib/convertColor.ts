68cc21ec62655bac58c1b8ba6358ba7399c2b8f1
Fayne Aldan <FayneAldan@gmail.com>
2022-12-30 09:00:11 -0700

    Update convertColor

--- lib/convertColor.ts
+++ lib/convertColor.ts
@@ -1,45 +1,61 @@
 import chroma from "chroma-js";
 
-const colorMap: Record<string, string> = {
-  "&0": "#000000",
-  "&8": "#555555",
-  "&1": "#0000AA",
-  "&9": "#5555FF",
-  "&2": "#00AA00",
-  "&a": "#55FF55",
-  "&3": "#00AAAA",
-  "&b": "#55FFFF",
-  "&4": "#AA0000",
-  dark_red: "#AA0000",
-  "&c": "#FF5555",
-  "&5": "#AA00AA",
-  "&d": "#FF55FF",
-  "&6": "#FFAA00",
-  "&e": "#FFFF55",
-  "&7": "#AAAAAA",
-  "&f": "#FFFFFF",
-  black: "#000000",
-  light_gray: "#ADADAD",
-  red: "#FF0000",
-  gray: "#636363",
-  green: "#00FF00",
-  pink: "#FFB7B7",
-  blue: "#0000FF",
-  light_green: "#49FF59",
-  yellow: "#FFFF00",
-  light_blue: "#00E9FF",
-  brown: "#563100",
-  magenta: "#FF0083",
-  purple: "#B200FF",
-  orange: "#FF9000",
-  cyan: "#438E82",
-  white: "#FFFFFF",
+const colorMap: Record<string, string | number> = {
+  // CommonColors
+  BLACK: 0x000000,
+  RED: 0xff0000,
+  DARK_RED: 0xaa0000,
+  GREEN: 0x00ff00,
+  BLUE: 0x0000ff,
+  YELLOW: 0xffff00,
+  BROWN: 0x563100,
+  PURPLE: 0xb200ff,
+  CYAN: 0x438e82,
+  LIGHT_GRAY: 0xadadad,
+  GRAY: 0x636363,
+  PINK: 0xffb7b7,
+  LIGHT_GREEN: 0x49ff59,
+  LIGHT_BLUE: 0x00e9ff,
+  MAGENTA: 0xff0083,
+  ORANGE: 0xff9000,
+  WHITE: 0xffffff,
+
+  GREY: 0x636363,
+  LIGHT_GREY: 0xadadad,
+
+  // CustomColor
+  DARK_BLUE: 0x0000aa,
+  DARK_GREEN: 0x00aa00,
+  DARK_AQUA: 0x55ffff,
+  DARK_PURPLE: 0xaa00aa,
+  GOLD: 0xffaa00,
+  DARK_GRAY: 0x555555,
+  AQUA: 0x55ffff,
+  LIGHT_PURPLE: 0xff55ff,
+
+  // MinecraftChatColors Aliases
+  DARK_CYAN: 0x00aaaa,
+  SILVER: 0xaaaaaa,
+  VIOLET: 0x5555ff,
+  PALE_GREEN: 0x55ff55,
 };
 
+const mcChatColors: number[] = [
+  0x000000, 0x0000aa, 0x00aa00, 0x00aaaa, 0xaa0000, 0xaa00aa, 0xffaa00,
+  0xaaaaaa, 0x555555, 0x5555ff, 0x55ff55, 0x55ffff, 0xff5555, 0xff55ff,
+  0xffff55, 0xffffff,
+];
+for (let i = 0; i < 16; i++) {
+  colorMap["&" + i] = mcChatColors[i];
+  colorMap["§" + i] = mcChatColors[i];
+}
+
 export function convertColor(input: string): string {
-  if (colorMap[input.toLowerCase()]) {
-    return chroma(colorMap[input.toLowerCase()]).hex();
+  const key = input.trim().replace(" ", "_").toLocaleUpperCase();
+  if (colorMap[key]) {
+    return chroma(colorMap[key]).hex();
   }
+
   if (input.startsWith("rgba(")) {
     const rgba = <[number, number, number, number]>input
       .substring(5, input.length - 1)
